0843d8dc02fd925baabf45f318a8a3a3b3d1b564
tuancuainfo2-dot <tuancuainfo2@gmail.com>
2025-12-18 02:34:40 +0700

    Add Layout component with children prop

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,1 +1,14 @@
+import React from "react";
 
+type Props = {
+  children: React.ReactNode;
+};
+
+const Layout: React.FC<Props> = ({ children }) => (
+  <div style={{ padding: 20 }}>
+    <h2>Layout</h2>
+    {children}
+  </div>
+);
+
+export default Layout;
